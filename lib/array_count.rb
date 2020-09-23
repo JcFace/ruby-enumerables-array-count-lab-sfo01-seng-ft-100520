34647ed860e8433e342234229b481d510fa96f23
Jesse Vaughn <jcv221@gmail.com>
2020-09-23 21:16:36 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -10,3 +10,4 @@ end
 
 # Return the total number of strings in the provided array using the count enumerable
 
+  # Return the total number of EMPTY strings in the provided array using the count enumerable
